60825b0e3d7d37d6bb397374ede944d4c43e217c
Piotr Macuk <piotr@macuk.pl>
2025-11-21 15:19:42 +0100

    Fix

--- light_trx.ino
+++ light_trx.ino
@@ -8,7 +8,7 @@ Servo servo;
 int servoPin = 10;      // servo signal pin
 int servoRxPos = 0;     // position for RX
 int servoTxPos = 95;   // position for TX
-int txPin = 9;          // TX pin -- should be pulled-down to GND with 10k resistor
+int txPin = 9;         // TX pin -- should be pulled-down to GND with 10k resistor
 
 void setup() {
   servo.attach(servoPin);
